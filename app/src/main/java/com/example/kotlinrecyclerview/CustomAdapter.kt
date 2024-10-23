c651b8f367f7d0ed04ecf4973dbffbcf84f4a67f
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-23 15:56:58 -0300

    agrego recyclerview

--- app/src/main/java/com/example/kotlinrecyclerview/CustomAdapter.kt
+++ app/src/main/java/com/example/kotlinrecyclerview/CustomAdapter.kt
@@ -9,26 +9,45 @@ import androidx.recyclerview.widget.RecyclerView
 
 class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
+    private val title = arrayOf(
+        "Chapter 1",
+        "Chapter 2",
+        "Chapter 3",
+        "Chapter 4"
+    )
+
+    private val details = arrayOf(
+        "Chapter 5",
+        "Chapter 6",
+        "Chapter 7",
+        "Chapter 8"
+    )
+
+    private val images = intArrayOf(
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground
+    )
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         var v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
-        viewHolder.itemTitle.text = titles[i]
-        viewHolder.itemDetail.text = titles[i]
-        viewHolder.itemImage.isetImageResource(images[i])
+        holder.itemTitle.text = title[i]
+        holder.itemDetail.text = details[i]
+        holder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return title.size
     }
 
     inner class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
-        var itemImage: ImageView
-        var itemTitle: TextView
-        var itemDetail: TextView
-
-        init {
-            itemImage = itemView.findViewById(R.id.item_image)
-            itemTitle = itemView.findViewById(R.id.item_title)
-            itemDetail = itemView.findViewById(R.id.item_detail)
-        }
+        var itemImage: ImageView = itemView.findViewById(R.id.item_image)
+        var itemTitle: TextView = itemView.findViewById(R.id.item_title)
+        var itemDetail: TextView = itemView.findViewById(R.id.item_detail)
     }
 }
